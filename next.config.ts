48c0e9029e2a72b2591b8edca7cd918f5ac4dc3e
Ayman <aymanakh99@gmail.com>
2026-07-14 10:37:32 +0300

    Initial portfolio sawebsite

--- next.config.ts
+++ next.config.ts
@@ -3,15 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
     output: "export",
-//     basePath:"/ayman-portfolio",
-
-// assetPrefix:"/ayman-portfolio/",
   images: {
     unoptimized: true,
   },
   basePath: "/portfolio",
-  assetPrefix: "/portfolio/",
-  
 };
 
 export default nextConfig;
